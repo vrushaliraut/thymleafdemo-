f1eea735f605b3ae6a26cf11af1067c0a08150c2
vrushali_raut <vraut.c@roshn.sa>
2025-02-26 14:28:02 +0530

    data binding using radio button in student-form.html

--- src/main/java/com/luv2code/springboot/thymleafdemo/controller/StudentController.java
+++ src/main/java/com/luv2code/springboot/thymleafdemo/controller/StudentController.java
@@ -16,6 +16,9 @@ public class StudentController {
     @Value("${countries}")
     private List<String> countries;
 
+    @Value("${languages}")
+    private List<String> languages;
+
     @GetMapping("/showStudentForm")
     public String showForm(Model theModel) {
 
@@ -28,6 +31,8 @@ public class StudentController {
         // Add the list of countries to the model
         theModel.addAttribute("countries", countries);
 
+        // Add the list of languages to the model
+        theModel.addAttribute("languages", languages);
 
         return "student-form";
     }
